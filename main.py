00d829c4fd1937d3a4bb07e599d06672100fff34
deniz-icin <denizicinn10@gmail.com>
2023-04-03 11:18:20 +0300

    exception added for invalid id

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def list_all_instances():
     success = is_response_successful(response)
     if success is False: print("Connection failed.")
     instances = EC2_RESOURCE.instances.all()
-    print()
     instance_string = [
         "Instance ID = {:^30s} |   Public IPv4 address: {:^15s} |   Instance State : {:>10s}".format(instance.id,str(instance.public_ip_address),instance.state['Name'])
         for instance in instances
@@ -23,38 +22,40 @@ def list_all_instances():
     if len(instance_string) == 0 :
         print("There is no instance running or stopped. \n")
         sys.exit()
-    else:
-        print("\n".join(instance_string))
+    print("\n".join(instance_string))
 
 def stop_instance(instance_id):
     instance = EC2_RESOURCE.Instance(instance_id)
-    # if instance_id != instance.id:
-    #     print("\n ID wrong! \n")
-    #     return get_input()
-    instance.stop()
-    print(f"Stopping EC2 instance: {instance_id}")
-    instance.wait_until_stopped()
-    print(f'EC2 instance "{instance_id}" has been stopped')
+    try:
+        instance.stop()
+        print(f"Stopping EC2 instance: {instance_id}")
+        instance.wait_until_stopped()
+        print(f'EC2 instance "{instance_id}" has been stopped')
+    except:
+        print("\n ID wrong! Please type valid instance ID. \n")
+        return get_input()
 
 def terminate_instance(instance_id):
     instance = EC2_RESOURCE.Instance(instance_id)
-    # if instance_id != instance.id:
-    #     print("\n ID wrong! \n")
-    #     return get_input()
-    instance.terminate()
-    print(f"Terminating EC2 instance: {instance_id}")
-    instance.wait_until_terminated()
-    print(f'EC2 instance "{instance_id}" has been terminated')
+    try:
+        instance.terminate()
+        print(f"Terminating EC2 instance: {instance_id}")
+        instance.wait_until_terminated()
+        print(f'EC2 instance "{instance_id}" has been terminated')
+    except:
+        print("\n ID wrong! Please type valid instance ID. \n")
+        return get_input()
 
 def start_instance(instance_id):
     instance = EC2_RESOURCE.Instance(instance_id)
-    # if instance_id != instance.id:
-    #     print("\n ID wrong! \n")
-    #     return get_input()
-    instance.start()
-    print(f"Starting EC2 instance: {instance_id}")
-    instance.wait_until_running()
-    print(f'EC2 instance "{instance_id}" has been started')
+    try:
+        instance.start()
+        print(f"Starting EC2 instance: {instance_id}")
+        instance.wait_until_running()
+        print(f'EC2 instance "{instance_id}" has been started')
+    except:
+        print("\n ID wrong! Please type valid instance ID. \n")
+        return get_input()
 
 def get_input():
     instance_id = input("You can start, stop or terminate your instances.\nFirst, which instance do you want to process?\nPlease enter instance id : ")
@@ -84,7 +85,6 @@ def do_again():
         return main()
     print("\n You can only type (y) or (n).")
     return do_again()
-
 
 if __name__ == "__main__":
     main()
